f484e1399384b7a8d0166fb51fa267c76ae488a5
Nikhil Abraham <nikhil.abraham2001@gmail.com>
2024-02-11 20:39:46 +0530

    v5

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ const Navbar = () => {
     <nav className="bg-slate-100 p-4 pt-4 maulline">
       <div className="container mx-auto flex items-center justify-between w-2/6">
         <a href="#" className="text-slate-700 font-normal text-xl mx-2">
-          Home V2
+          Home V2.1
         </a>
         <a
           href="#"
